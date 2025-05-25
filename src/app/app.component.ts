352e60dfb6c4e0d7130fc0b287630e9a72c6b09b
Prueba <tony270604@gmail.com>
2025-05-25 18:42:30 -0500

    establecimos ejemplo de como usar las rutas, el sass y el sass general

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None // Desactiva el encapsulamiento
+
 })
 export class AppComponent {
   title = 'Angular';
---
+++ src/app/components/administrador/barraizq/barraizq.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barraizq',
+  imports: [],
+  templateUrl: './barraizq.component.html',
+  styleUrl: './barraizq.component.scss'
+})
+export class BarraizqComponent {
+
+}
